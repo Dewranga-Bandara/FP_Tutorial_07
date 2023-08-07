fb9f2721dd9b8ce722f5f7e96ad9b62523549ee8
Dewranga Bandara <dewrangabandara071@gmail.com>
2023-08-08 00:52:23 +0530

    Initial commit

---
+++ src/main/scala/Q2.scala
@@ -0,0 +1,11 @@
+object Q2 {
+  def calculateSquare(numbers: List[Int]): List[Int] = {
+    numbers.map(num => num * num)
+  }
+
+  def main(args: Array[String]): Unit = {
+    val inputList = List(1, 2, 3, 4, 5)
+    val outputList = calculateSquare(inputList)
+    println(outputList)
+  }
+}
